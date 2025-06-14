771691386919d69121b5e9fc4db40b31fdf288a4
Gri <makiev.grisha@gmail.com>
2025-06-14 20:28:21 +0300

    дабавил загрузку файла

---
+++ controllers/AjoController.php
@@ -0,0 +1,40 @@
+<?php
+namespace app\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\UploadedFile;
+use yii\web\Response;
+
+class AjoController extends Controller
+{
+    public function actionIndex()
+    {
+        $filePath = '/home/limkorm-check-bot/upload/AJO.xlex';
+        $fileContent = file_exists($filePath) ? file_get_contents($filePath) : null;
+
+        return $this->render('index', [
+            'fileContent' => $fileContent,
+        ]);
+    }
+
+    public function actionUpload()
+    {
+        $uploadPath = '/home/limkorm-check-bot/upload/';
+        $fileName = 'AJO.xlex';
+
+        $uploadedFile = UploadedFile::getInstanceByName('ajoFile');
+        if ($uploadedFile && $uploadedFile->name === $fileName) {
+            $fullPath = $uploadPath . $fileName;
+            if ($uploadedFile->saveAs($fullPath)) {
+                Yii::$app->session->setFlash('success', 'Файл успешно загружен и сохранён.');
+            } else {
+                Yii::$app->session->setFlash('error', 'Ошибка при сохранении файла.');
+            }
+        } else {
+            Yii::$app->session->setFlash('error', 'Пожалуйста, выберите файл с именем AJO.xlex.');
+        }
+
+        return $this->redirect(['index']);
+    }
+}
